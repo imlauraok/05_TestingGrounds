1f58e05fdf2bebfb8973a0ab62f11c2d1f4a8da8
Mimikyu <imlauraok@gmail.com>
2019-01-31 12:01:08 +0000

    Converting Blueprint to C++

--- Source/TestingGrounds/ChooseNextWaypoint.h
+++ Source/TestingGrounds/ChooseNextWaypoint.h
@@ -21,4 +21,7 @@ class TESTINGGROUNDS_API UChooseNextWaypoint : public UBTTaskNode
 protected:
 	UPROPERTY(EditAnywhere, Category = "Blackboard")
 	struct FBlackboardKeySelector IndexKey;
+
+	UPROPERTY(EditAnywhere, Category = "Blackboard")
+	struct FBlackboardKeySelector WaypointKey;
 };
